9b744ab4cf6f251ebc00b7a5c436c9ea06c96220
millymats2 <97088594+millymats2@users.noreply.github.com>
2023-04-10 15:33:19 -0700

    fixed resume button

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,7 @@
 import './index.scss';
 import { NavLink } from 'react-router-dom';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
     return(
@@ -9,8 +10,11 @@ const Home = () => {
                 <h1 class="name">MILANA MATSUKA</h1>
                <p> <h1 class="words">DEVELOPER. CREATOR. VISIONARY.</h1></p>
                <NavLink exact="true" activeclassname="active" className="resume-link" to="/resume">
-                <p><h1 class="resume">RESUME</h1></p>
+                <h1 class="resume"><div class="inner">RESUME</div></h1>
             </NavLink>
+            <div class="icons">
+                <h1>Skills</h1>
+                <h1 class="icon"><FontAwesomeIcon icon={faEnvelope} color="#000000"/></h1></div>
             </div>
         </div>
     )
